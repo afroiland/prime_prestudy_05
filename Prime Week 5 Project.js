767d828dccf9ed0180e8cfd0769ac2c1553c2953
Andrew Froiland <afroiland@Andrews-MacBook-Pro.local>
2016-10-16 12:28:22 -0500

    Initial Commit

---
+++ Prime Week 5 Project.js
@@ -0,0 +1,17 @@
+
+//avengersMovie = {
+//    name : Avengers;
+//    runtime : 143 mins;
+//    releaseYear = 2012;
+//    "Director" : "Joss Whedon".
+//}
+
+var avengersMovie = {
+  name:"Avengers", runtime:143, releaseYear:2012, director:"Joss Whedon"
+};
+
+var showObject=function(){
+  console.log(avengersMovie);
+};
+
+showObject();
